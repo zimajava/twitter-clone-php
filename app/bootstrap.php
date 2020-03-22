df010c8b63c833c0a0c3a76dc6cd2043516b8514
Zima Maksim <maksimz@wizardsdev.com>
2020-03-23 01:09:36 +0200

    Initial commit

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,12 +1,19 @@
-<?php 
+<?php
+
+    // App Root
+    define('APPROOT', __DIR__);
+// URL Root
+    define('URLROOT', 'http://twitter.local:8888');
+// Site Name
+    define('SITENAME', 'Twitter Clone');
 // Load Config
-require_once './config/config.php';
+    require_once(APPROOT.'/config/config.php');
 // Load Helpers
-require_once './helpers/functions.php';
-require_once './helpers/auth.php';
-require_once './helpers/validation.php';
+    require_once(APPROOT.'/helpers/functions.php');
+    require_once(APPROOT.'/helpers/auth.php');
+    require_once(APPROOT.'/helpers/validation.php');
 
 // Autoload Core Libraries
-spl_autoload_register(function($className) {
-  require_once './libraries/'. $className .'.php';
-});
+    spl_autoload_register(function($className) {
+        require_once(APPROOT.'/libraries/'. $className .'.php');
+    });
--- app/config/config.php
+++ app/config/config.php
@@ -1,36 +1,22 @@
-<?php 
-ob_start();
-session_start();
+<?php
+    ob_start();
+    session_start();
 
 // DB Params
-//define('DB_HOST', 'localhost');
-//define('DB_USER', 'test');
-//define('DB_PASS', 'test');
-//define('DB_NAME', 'twitter');
-
     $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
     $host = $url['host'] ?? 'localhost';
     $username = $url['user'] ?? 'test';
     $password = $url['pass'] ?? 'test';
     $db = substr($url['path'], 1);
     $database = strlen($db) > 0 ? $db : 'twitter';
-//    echo '<pre>';
-//    print_r($host);
-//    echo '</br>';
-//    print_r($username);
-//    echo '</br>';
-//    print_r($password);
-//    echo '</br>';
-//    print_r($database);
-//    echo '</pre>';
     define('DB_HOST', $host);
     define('DB_USER', $username);
     define('DB_PASS', $password);
     define('DB_NAME', $database);
 
-// App Root 
-define('APPROOT', dirname(__DIR__));
-// URL Root
-define('URLROOT', 'http://twitter.local:8888');
-// Site Name
-define('SITENAME', 'Twitter Clone');
+//// App Root
+//    define('APPROOT', dirname(dirname(__FILE__)));
+//// URL Root
+//    define('URLROOT', 'http://twitter.local:8888');
+//// Site Name
+//    define('SITENAME', 'Twitter Clone');
