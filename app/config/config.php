c082a975b66aca7df023953664d827d705c1f49d
Zima Maksim <maksimz@wizardsdev.com>
2020-03-23 00:29:01 +0200

    Initial commit

--- app/config/config.php
+++ app/config/config.php
@@ -3,10 +3,30 @@ ob_start();
 session_start();
 
 // DB Params
-define('DB_HOST', 'localhost');
-define('DB_USER', 'test');
-define('DB_PASS', 'test');
-define('DB_NAME', 'twitter');
+//define('DB_HOST', 'localhost');
+//define('DB_USER', 'test');
+//define('DB_PASS', 'test');
+//define('DB_NAME', 'twitter');
+
+    $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+    $host = $url['host'] ?? 'localhost';
+    $username = $url['user'] ?? 'test';
+    $password = $url['pass'] ?? 'test';
+    $db = substr($url['path'], 1);
+    $database = strlen($db) > 0 ? $db : 'twitter';
+//    echo '<pre>';
+//    print_r($host);
+//    echo '</br>';
+//    print_r($username);
+//    echo '</br>';
+//    print_r($password);
+//    echo '</br>';
+//    print_r($database);
+//    echo '</pre>';
+    define('DB_HOST', $host);
+    define('DB_USER', $username);
+    define('DB_PASS', $password);
+    define('DB_NAME', $database);
 
 // App Root 
 define('APPROOT', dirname(__DIR__));
--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php 
 require_once('../app/bootstrap.php');
-
+require('../vendor/autoload.php');
 // Init Core Library
 $init = new Core;
